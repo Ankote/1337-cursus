02676deef5ff02a4e9e77c1bfa09a95d7ec48ed7
Ayoub Ankote <aankote@e1r6p4.1337.ma>
2022-10-19 08:08:37 +0100

    some edites and add function ft_split.c

---
+++ libft/ft_split.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aankote <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 14:37:08 by aankote           #+#    #+#             */
+/*   Updated: 2022/10/17 14:37:12 by aankote          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int first_alloc(char const *s, char c)
+{
+    int i;
+    int cpt;
+    int test;
+    char **p;
+
+    i =0;
+    test = 0;
+    cpt = 0;
+    if (s)
+    {
+    while (s[i])
+    {
+        if(s[i] != c && test == 0)
+        {
+            test = 1;
+            cpt ++;
+        }
+        if (s[i] == c)
+         test = 0;
+        i++;
+    }
+       if((p = (char **)malloc(sizeof(char *) * (cpt + 1))))
+       return (p);
+    }
+    return (0);
+}
+
+char **ft_split(char const *s, char c)
+{
+    unsigned int i;
+    int start;
+    int k;
+    char **p;
+
+    i = 0;
+    start = -1;
+    k = 0;
+    if ((p = first_alloc(s,c)))
+    {
+        while (i <= strlen(s)) 
+        {
+            if (s[i] != c && start == -1) 
+                start = i;
+            else if ((s[i] == c || i == strlen(s)) && start >= 0)
+            {
+                p[k++] = ft_substr(s,start,i - start);
+                start = -1;
+            }
+        i++;
+        }
+        p[k] = NULL;  
+    }
+}
+
